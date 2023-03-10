2b9a54fb2c16322801c6c49622bb55646726913c
petrosmp <petrakis798@gmail.com>
2023-03-10 21:58:21 +0200

    Improved test script

--- assignment_1/test.py
+++ assignment_1/test.py
@@ -2,25 +2,24 @@ import platform
 import subprocess
 
 polynomials = [
-    [8, 3, 6, 2, 0, 12],
-    [3, -124, 439, 1234, -10439, 931678],
-    [4, -1, -5, 13, -17, 12, 4],
+    [8, 3, 6, 2, 0, 12],                    # root = -1
+    [3, -124, 439, 1234, -10439, 931678],   # root ~ 37
+    [1, -42, 861, -9534, 60696, -149688],   # root ~ 5 
+    [1, -42, 1, -40, -78, -246],            # root = 42
+    [1, -1337, 1, -1335, -2668, -8023]      # root = 1337
 ]
-deltas = [0.1, 0.001]
+delta = 0.001
 x0s = [0, 1, 10000000]
-iters = [20, 100, 10000]
+iters = [20, 100000]
 executable = "./find_roots.exe" if platform.system() == "Windows" else "./find_roots"
 
 for polynomial in polynomials:
-    for delta in deltas:
+    for x0 in x0s:
         for iter in iters:
-            for x0 in x0s:
-                args = f"{executable} {' '.join([str(x) for x  in polynomial])} -d {delta} -i {iter} -x {x0}".split()
-                popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-                popen.wait()
-                output = popen.stdout.read().decode()
-                print(output)
+            print(f"p{polynomials.index(polynomial)+1}, x0={x0}, max_iters={iter}")
+            args = f"{executable} {' '.join([str(x) for x  in polynomial])} -d {delta} -i {iter} -x {x0}".split()
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            output = popen.stdout.read().decode()
+            print(output)
     print("==============================================================================")
-
-
-
