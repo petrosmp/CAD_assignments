dd3301e2da2c8f197838ad47daaed7a1fe753a94
petrosmp <petrakis798@gmail.com>
2023-04-03 06:32:11 +0300

    Tested create_custom()

--- assignment_3/subsys_test.c
+++ assignment_3/subsys_test.c
@@ -8,6 +8,10 @@
 #define SUBSYS_LIB_NAME "subsystem.lib"
 #define SIZE 100
 
+#define A "A0"
+#define B "B0"
+#define C "Cin"
+
 int main() {
 
 
@@ -63,6 +67,49 @@ int main() {
     }
 
 
+    char *a[] = {A,B,C};
+    for(int i=0; i<3; i++) printf("%s\n", a[i]);
+
+    Subsystem *custom_FA = malloc(sizeof(Subsystem));
+    int next = create_custom(custom_FA, lib->contents->std, 3, a, 7);
+
+    c = custom_FA->components;
+
+    while (c != NULL) {
+
+        Component *comp = c->comp;
+    
+        printf("\tU%d: %s,", comp->id, comp->prototype->gate->name);
+        printf(" defined in %s", comp->prototype->defined_in->file);
+        printf(", with %d inputs ", comp->prototype->gate->_inputc);
+
+        for (int i=0; i<comp->prototype->gate->_inputc; i++)  {
+            printf("%s ", comp->prototype->gate->inputs[i]);
+        }
+
+        printf("mapped to ");
+        for (int i=0; i<comp->_inputc; i++) {
+            printf("%s ", comp->inputs[i]);
+        }
+        printf("respectively\n");
+
+        c = c->next;
+
+    }
+
+    // print the output mappings
+    printf("and the following output mappings:\n");
+
+    for(int i=0; i<custom_FA->_outputc; i++) {
+        printf("\t%s <- %s\n", custom_FA->outputs[i], custom_FA->output_mappings[i]);
+    }
+
+    printf("next component will be U%d\n", next);
+    printf("\n");
+
+
+
+    free_subsystem(custom_FA);
     free_lib(lib);
     free_lib(gate_lib);
     free(buf);
